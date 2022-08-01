687fcf9463224014f1763d5c79f1f0af67d45265
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-08-01 20:49:38 +0100

    Add files via upload

--- main.rb
+++ main.rb
@@ -17,7 +17,7 @@ def mod1()
 
     answer = gets.chomp()
 
-    if answer == "DC" 
+    if answer.upcase() == "DC" 
         puts "Correct!"
     else
         puts "Incorrect! The current of batteries always flows in the same direction!"
@@ -490,7 +490,7 @@ def quiz()
 
     # Question 4
 
-    puts "Question 2 - Are LEDs polarised [Y/N]?" 
+    puts "Question 4 - Are LEDs polarised [Y/N]?" 
 
     answer4 = gets.chomp()
 
@@ -504,12 +504,374 @@ def quiz()
 
     puts "Question 5 - What is the formulae for calculating resistance (letters only, no spaces)?" 
 
-    answer2 = gets.chomp()
+    answer5 = gets.chomp()
 
-    if answer2.downcase() == "r=v/i"
+    if answer5.downcase() == "r=v/i"
         puts "Correct!"
     else
         puts "Incorrect! R = V / I (Resistance = Voltage / Ampere (or current)"
+    end
+end
+
+def mod1w()
+    puts "---------------------------------"
+    puts "HTML5 Basics"
+    puts "---------------------------------"
+    puts "HTML is an acronym for Hypertext Markup Language."
+    sleep 4
+    puts "HTML is used for content on web pages."
+    sleep 4
+    puts "The <p></p> tags are used for paragraphs."
+    sleep 4
+    puts "The <h1></h1> tags are used for headers. The number can be changed for different sizes."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What are the tags used for paragraphs?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "<p></p>" 
+        puts "Correct!"
+    else
+        puts "Incorrect! The tags used for making a paragraph are <p></p>"
+    end
+end
+
+def mod2w()
+    puts "---------------------------------"
+    puts "HTML5 Semantic Tags"
+    puts "---------------------------------"
+    puts "HTML semantic tags describe the content inside of them."
+    sleep 4
+    puts "For example, if you have an article, you would use the <article</article> tags."
+    sleep 4
+    puts "Another example of these tags is for navigation, where you can use the <nav></nav>."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What do semantic tags do?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase().include?("describe the content inside")
+        puts "Correct!"
+    else
+        puts "Incorrect! Semantic tags describe the content inside of them."
+    end
+end
+
+def mod3w()
+    puts "---------------------------------"
+    puts "HTML5 Buttons & Links"
+    puts "---------------------------------"
+    puts "To define buttons in HTML, use the <button></button> tags."
+    sleep 4
+    puts "For a link, use the <a></a> tags."
+    sleep 4
+    puts "The tags for links will need a href attribute, which will be the link."
+    sleep 4
+    puts "So in the end, a link would like like: <a href='link'>Content</a>"
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What attribute do links need?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase().include?("href")
+        puts "Correct!"
+    else
+        puts "Incorrect! Links need the href attribute inside of them."
+    end
+end
+
+def mod4w()
+    puts "---------------------------------"
+    puts "HTML5 Images"
+    puts "---------------------------------"
+    puts "To make an image in HTML, use the <img> tag."
+    sleep 4
+    puts "You will need to use the src attribute for your image."
+    sleep 4
+    puts "You can also use the alt attribute so if your image doesn't load, people will know what it is."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What is the tag for images?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "<img>"
+        puts "Correct!"
+    else
+        puts "Incorrect! The tag for images is <img>"
+    end
+end
+
+def mod5w()
+    puts "---------------------------------"
+    puts "HTML5 Lists"
+    puts "---------------------------------"
+    puts "There are two types of lists in HTML, ordered and unordered lists."
+    sleep 4
+    puts "Unordered lists use bullet points. Ordered lists use numbers."
+    sleep 4
+    puts "To make an unordered list, use <ul></ul>. To make an ordered list, use <ol></ol>"
+    sleep 4
+    puts "You will also need to have items in the list. Use the list item tag for that (<li></li>)."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What are the tags for unordered lists?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "<ul></ul>"
+        puts "Correct!"
+    else
+        puts "Incorrect! The tags for unordered lists are <ul></ul>"
+    end
+
+    puts "---------------------------------"
+    puts "Do unordered lists use numbers [Y/N]?" 
+
+    answer2 = gets.chomp()
+
+    if answer2.downcase() == "N"
+        puts "Correct!"
+    else
+        puts "Incorrect! Unordered lists have no order so they use bullet points."
+    end
+end
+
+def mod6w()
+    puts "---------------------------------"
+    puts "Classes & IDs"
+    puts "---------------------------------"
+    puts "To style our websites, we need to use CSS (covered in the next modules)."
+    sleep 4
+    puts "However, if we want to access specific elements, like a button, we need to use the button {} selector."
+    sleep 4
+    puts "We can also use classes. Classes allow us to style specifc elements with the class. Use the class='' attribute."
+    sleep 4
+    puts "We can also use IDs. IDs allow us to style one specifc element with the ID. Use the id='' attribute."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "How many elements do IDs let us style (in words)?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "one"
+        puts "Correct!"
+    else
+        puts "Incorrect! IDs only allow us to style one element. Classes let us style many elements."
+    end
+
+    puts "---------------------------------"
+    puts "What is the syntax for a button selector?" 
+
+    answer2 = gets.chomp()
+
+    if answer2.downcase() == "button {}"
+        puts "Correct!"
+    else
+        puts "Incorrect! The syntax looks like: button {}"
+    end
+end
+
+def mod7w()
+    puts "---------------------------------"
+    puts "CSS basics"
+    puts "---------------------------------"
+    puts "CSS stands for Cascading Style Sheet."
+    sleep 4
+    puts "We use CSS to style our websites."
+    sleep 4
+    puts "We can style specific elements with selectors (covered in module 6)."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What does CSS stand for?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "cascading style sheet"
+        puts "Correct!"
+    else
+        puts "Incorrect! CSS stands for cascading style sheets."
+    end
+
+    puts "---------------------------------"
+    puts "Can we use CSS to style our website [Y/N]?" 
+
+    answer2 = gets.chomp()
+
+    if answer2.downcase() == "Y"
+        puts "Correct!"
+    else
+        puts "Incorrect! We can use CSS to style our website."
+    end
+end
+
+def mod8w()
+    puts "---------------------------------"
+    puts "CSS colours"
+    puts "---------------------------------"
+    puts "We can use colours in CSS to add colours to elements."
+    sleep 4
+    puts "Use the background-color: <colour> style to add a background colour."
+    sleep 4
+    puts "Use the color: <colour> style to add a foreground colour."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What is the syntax for background colours in CSS?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase().include?("background-color:")
+        puts "Correct!"
+    else
+        puts "Incorrect! We need to use the background-color: style to add a background colour."
+    end
+
+    puts "---------------------------------"
+    puts "Can we use the color: style to add a background colour [Y/N]?" 
+
+    answer2 = gets.chomp()
+
+    if answer2.downcase() == "N"
+        puts "Correct!"
+    else
+        puts "Incorrect! We can use the background-color: style to add a background colour."
+    end
+end
+
+def mod9w()
+    puts "---------------------------------"
+    puts "CSS :hover()"
+    puts "---------------------------------"
+    puts "The :hover() selector in CSS is known as a pseudoselector."
+    sleep 4
+    puts "This pseudoselector allows us to add a style to an element when it is hovered over."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What is :hover() known as? A..." 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "pseudoselector"
+        puts "Correct!"
+    else
+        puts "Incorrect! It is known as a pseudoselector."
+    end
+end
+
+def mod10w()
+    puts "---------------------------------"
+    puts "CSS Animations"
+    puts "---------------------------------"
+    puts "We can use CSS animations to add an animation."
+    sleep 4
+    puts "Just use @keyframes <animationName> {}."
+    sleep 4
+    puts "In the curly braces, you can add in different styles. You must, however, add percentages."
+    sleep 4
+    puts "For example, @keyframes myAnimation { 0% {background-color: blue}, 100% {background-color: orange} };"
+    sleep 4
+    puts "You then need to add the animation with the animation: style to an element."
+    sleep 4
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+    puts "What is the syntax for an animation?" 
+
+    answer = gets.chomp()
+
+    if answer.downcase() == "@keyframes <animationName> {}"
+        puts "Correct!"
+    else
+        puts "Incorrect! The syntax for an animation is @keyframes <animationName> {}"
+    end
+end
+
+def quizw()
+    puts "---------------------------------"
+    puts "Quiz Time!"
+    puts "---------------------------------"
+
+    # Question 1
+
+    puts "Question 1 - What does HTML stand for?" 
+
+    answer1 = gets.chomp()
+
+    if answer1.downcase() == "hypertext markup language"
+        puts "Correct!"
+        sleep 4
+    else
+        puts "Incorrect! HTML stands for Hyptertext Markup Language!"
+        sleep 4
+    end
+
+    # Question 2
+
+    puts "Question 2 - What does CSS stand for?" 
+
+    answer2 = gets.chomp()
+
+    if answer2.downcase() == "cascading style sheet"
+        puts "Correct!"
+    else
+        puts "Incorrect! CSS stands for Cascading Style Sheet."
+    end
+
+    # Question 3
+
+    puts "Question 3 - What are the tags for unordered lists?" 
+
+    answer3 = gets.chomp()
+
+    if answer3.downcase() == "<ul></ul>"
+        puts "Correct!"
+    else
+        puts "Incorrect! The tags are <ul</ul>"
+    end
+
+    # Question 4
+
+    puts "Question 4 - How can I add a background colour to an element?" 
+
+    answer4 = gets.chomp()
+
+    if answer4.downcase() == "background-color: <colour>"
+        puts "Correct!"
+    else
+        puts "Incorrect! The syntax for adding a background colour is background-color: <colour>"
+    end
+
+    # Question 5
+
+    puts "Question 5 - What is the tag for an image?" 
+
+    answer5 = gets.chomp()
+
+    if answer5.downcase() == "<img>"
+        puts "Correct!"
+    else
+        puts "Incorrect! The tag for an image is <img>"
     end
 end
 
@@ -585,7 +947,7 @@ while true do
         end
     elsif option == "2"
         puts "---------------------------------"
-        puts "Web Design (0/12)"
+        puts "Web Design (9/11)"
         puts "---------------------------------"
         puts "Module 1 - HTML5 Basics"
         sleep 0.5
@@ -595,50 +957,46 @@ while true do
         sleep 0.5
         puts "Module 4 - HTML5 Images"
         sleep 0.5
-        puts "Module 5 - Lists"
+        puts "Module 5 - HTML5 Lists"
         sleep 0.5
         puts "Module 6 - Classes & ID"
         sleep 0.5
         puts "Module 7 - CSS Basics"
         sleep 0.5
-        puts "Module 8 - CSS Selectors"
+        puts "Module 8 - CSS Colours"
         sleep 0.5
-        puts "Module 9 - CSS Colours"
+        puts "Module 9 - CSS :hover()"
         sleep 0.5
-        puts "Module 10 - CSS :hover()"
-        sleep 0.5
-        puts "Module 11 - CSS Animations"
+        puts "Module 10 - CSS Animations"
         sleep 0.5
         puts "Quiz - See how much you know!"
 
         puts "\nChoose a module: "
-        moduleChosen = gets.chomp.to_i()
+        moduleChosen2 = gets.chomp.to_i()
 
-        case moduleChosen
+        case moduleChosen2
         when 1
-            mod1()
+            mod1w()
         when 2
-            mod2()
+            mod2w()
         when 3
-            mod3()
+            mod3w()
         when 4
-            mod4()
+            mod4w()
         when 5
-            mod5()
+            mod5w()
         when 6
-            mod6()
+            mod6w()
         when 7
-            mod7()
+            mod7w()
         when 8
-            mod8()
+            mod8w()
         when 9
-            mod9()
+            mod9w()
         when 10
-            mod10()
-        when 11
-            mod11()
+            mod10w()
         when "Quiz"
-            quiz()
+            quizw()
         end
     end
 end
